bc78ee5f551b64ecdaeba03ef90f64878185bf64
Felipe Desiderati <felipedesiderati@gmail.com>
2020-01-07 14:10:08 -0300

    Changed package name from 'br.tech.desiderati' to 'io.herd'.

--- demo-element-insurance/demo-element-insurance-server/src/main/java/br/tech/desiderati/demo/element/domain/InsuranceType.java
+++ demo-element-insurance/demo-element-insurance-server/src/main/java/br/tech/desiderati/demo/element/domain/InsuranceType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 - Felipe Desiderati
+ * Copyright (c) 2020 - Felipe Desiderati
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -18,7 +18,7 @@
  */
 package br.tech.desiderati.demo.element.domain;
 
-import br.tech.desiderati.common.data.jpa.Identity;
+import io.herd.common.data.jpa.Identity;
 import lombok.*;
 
 import javax.persistence.*;
